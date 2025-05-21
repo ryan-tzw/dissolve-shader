b1877da05ad5239a0761eef068d706a3481c6303
ryanteozw <ryanteozw@gmail.com>
2025-05-21 23:46:08 +0800

    reposition camera

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Experience from './Experience.jsx'
 
 export default function App() {
     return (
-        <Canvas shadows="soft" camera={{ zoom: 2 }}>
+        <Canvas shadows="soft" camera={{ zoom: 1.5, position: [1.5, 0, 4] }}>
             <Experience />
         </Canvas>
     )
